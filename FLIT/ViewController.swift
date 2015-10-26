894e0b67411df089496d60692c47409ac19be2e3
Ethan <ethan@mobileapplabfontbonne2s-imac.local>
2015-10-26 17:11:26 -0500

    Initial Commit

---
+++ FLIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FLIT
+//
+//  Created by Ethan on 10/26/15.
+//  Copyright © 2015 Ethan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
